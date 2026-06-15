694563db321a6015733189611742f446667ba9c8
Shuyeb82 <it23047@mbstu.ac.bd>
2026-06-15 10:51:13 +0600

    Add files via upload

---
+++ LabTest/Shape.java
@@ -0,0 +1,37 @@
+
+public abstract class Shape {
+
+    private String color;
+    private boolean filled;
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+
+    public String getColor() {
+        return color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "Color : " + color + "\n" +
+               "Filled: " + (filled ? "Yes" : "No");
+    }
+}
